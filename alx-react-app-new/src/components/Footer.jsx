5097f05a7e4526176470f9d88a3a987d3cec2be9
Hamza Fafouri <Hamfaf.fafouri1@gmail.com>
2025-11-07 17:13:53 +0100

    add styling

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer>
+         <footer style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
             <p>© 2023 City Lovers</p>
         </footer>
     );
