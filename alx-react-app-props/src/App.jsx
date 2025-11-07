a7271c5c662a7d5aba6768c53421a72e7ba17b32
Hamza Fafouri <Hamfaf.fafouri1@gmail.com>
2025-11-07 17:25:47 +0100

    add refactoring Task

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -8,7 +8,7 @@ import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
 import ProfilePage from "./ProfilePage";
-import UserContext from "./UserContext";
+import UserContext from "./components/UserContext";
 
 
 
