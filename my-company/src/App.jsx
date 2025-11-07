1bf9febe58465846f8c824170bcf1a61ae11655b
Hamza Fafouri <Hamfaf.fafouri1@gmail.com>
2025-11-07 17:39:03 +0100

    done

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -5,10 +5,10 @@ import './App.css'
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
 function App() {
   const [count, setCount] = useState(0)
 
