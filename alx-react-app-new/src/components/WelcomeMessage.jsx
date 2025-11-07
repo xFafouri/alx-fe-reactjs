2bfebf050858433716ebfe28c642f5d6e8a3c7ac
Hamza Fafouri <Hamfaf.fafouri1@gmail.com>
2025-11-07 17:12:50 +0100

    add styling

---
+++ alx-react-app-new/src/components/WelcomeMessage.jsx
@@ -0,0 +1,11 @@
+function WelcomeMessage() {
+    return (
+        <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>This is a simple JSX component.</p>
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
