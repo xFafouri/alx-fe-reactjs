73b3f5d7c93460c51c9492b62ed0c91734305be1
Hamza Fafouri <hamfaf.fafouri1@gmail.com>
2025-11-14 16:01:16 +0100

    fix second ex01

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,11 +1,30 @@
-import create from 'zustand'
+import create from 'zustand';
 
 const useRecipeStore = create(set => ({
   recipes: [],
-  addRecipe: (newRecipe) => set(state => ({
-    recipes: [...state.recipes, { ...newRecipe, id: Date.now().toString() }]
-  })),
-  setRecipes: (recipes) => set({ recipes })
+
+  // Add a new recipe
+  addRecipe: (newRecipe) =>
+    set(state => ({
+      recipes: [...state.recipes, { ...newRecipe, id: Date.now().toString() }]
+    })),
+
+  // Replace the entire recipes array
+  setRecipes: (recipes) => set({ recipes }),
+
+  // Update a single recipe by id
+  updateRecipe: (updatedRecipe) =>
+    set(state => ({
+      recipes: state.recipes.map(r =>
+        r.id === updatedRecipe.id ? { ...r, ...updatedRecipe } : r
+      )
+    })),
+
+  // Delete a recipe by id
+  deleteRecipe: (id) =>
+    set(state => ({
+      recipes: state.recipes.filter(r => r.id !== id)
+    }))
 }));
 
 export default useRecipeStore;
