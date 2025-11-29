bb43f315551ee398af55c61e49d268b359f2867b
xFafouri <hamfaf.fafouri1@gmail.com>
2025-11-29 13:42:46 +0000

    add userProfile

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from "./components/UserProfile";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,6 +26,9 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <div>
+        <UserProfile />
+        </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
