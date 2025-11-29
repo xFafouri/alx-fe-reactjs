c02ac041a5305df3cf08cf87eb9ec56977b23e0e
xFafouri <hamfaf.fafouri1@gmail.com>
2025-11-29 14:04:31 +0000

    last ex

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,16 @@
 function UserProfile() {
   return (
-    <div className="
-      bg-gray-100 
-      p-4 sm:p-4 md:p-8 
-      max-w-xs sm:max-w-xs md:max-w-sm 
-      mx-auto my-20 
-      rounded-lg shadow-lg
-    ">
+    <div
+      className="
+        bg-gray-100 
+        p-4 sm:p-4 md:p-8 
+        max-w-xs sm:max-w-xs md:max-w-sm 
+        mx-auto my-20 
+        rounded-lg shadow-lg
+        transition-shadow duration-300 ease-in-out
+        hover:shadow-xl
+      "
+    >
       <img
         src="https://via.placeholder.com/150"
         alt="User"
@@ -16,17 +20,19 @@ function UserProfile() {
           sm:w-24 sm:h-24 
           md:w-36 md:h-36 
           mx-auto
+          transition-transform duration-300 ease-in-out
+          hover:scale-110
         "
       />
 
       <h1
         className="
-          text-lg 
-          sm:text-lg 
-          md:text-xl 
+          text-lg sm:text-lg md:text-xl 
           text-blue-800 
           my-4 
           text-center
+          transition-colors duration-300 ease-in-out
+          hover:text-blue-500
         "
       >
         John Doe
@@ -34,9 +40,7 @@ function UserProfile() {
 
       <p
         className="
-          text-sm 
-          sm:text-sm 
-          md:text-base 
+          text-sm sm:text-sm md:text-base 
           text-gray-600 
           text-center
         "
