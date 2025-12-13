7b842a2317d7194908dd715910173a0eba8e171c
xFafouri <hamfaf.fafouri1@gmail.com>
2025-12-13 18:28:53 +0100

    Form Handling

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -4,45 +4,38 @@ import * as Yup from "yup";
 const validationSchema = Yup.object({
   username: Yup.string().required("Username is required"),
   email: Yup.string()
-    .email("Invalid email format")
+    .email("Invalid email")
     .required("Email is required"),
   password: Yup.string()
     .min(6, "Password must be at least 6 characters")
-    .required("Password is required")
+    .required("Password is required"),
 });
 
-function FormikForm() {
-  const initialValues = {
-    username: "",
-    email: "",
-    password: ""
-  };
-
-  const handleSubmit = async (values, { resetForm }) => {
-    // Mock API call
-    console.log("Sending data to API:", values);
-
-    alert("User registered successfully!");
-    resetForm();
-  };
-
+function formikForm() {
   return (
     <Formik
-      initialValues={initialValues}
+      initialValues={{
+        username: "",
+        email: "",
+        password: "",
+      }}
       validationSchema={validationSchema}
-      onSubmit={handleSubmit}
+      onSubmit={(values) => {
+        console.log(values);
+        alert("Formik form submitted!");
+      }}
     >
       <Form>
         <h2>Register (Formik)</h2>
 
         <Field name="username" placeholder="Username" />
-        <ErrorMessage name="username" component="p" style={{ color: "red" }} />
+        <ErrorMessage name="username" component="p" />
 
         <Field name="email" type="email" placeholder="Email" />
-        <ErrorMessage name="email" component="p" style={{ color: "red" }} />
+        <ErrorMessage name="email" component="p" />
 
         <Field name="password" type="password" placeholder="Password" />
-        <ErrorMessage name="password" component="p" style={{ color: "red" }} />
+        <ErrorMessage name="password" component="p" />
 
         <button type="submit">Register</button>
       </Form>
@@ -50,4 +43,4 @@ function FormikForm() {
   );
 }
 
-export default FormikForm;
+export default formikForm;
