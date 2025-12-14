5c8979169197da6ec8d076c77831249864814521
xFafouri <hamfaf.fafouri1@gmail.com>
2025-12-14 14:01:06 +0100

    fix bugs

--- react-todo/src/jest.config.cjs
+++ react-todo/src/jest.config.cjs
@@ -1,7 +1,4 @@
 module.exports = {
   testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.[jt]sx?$": "babel-jest"
-  },
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"]
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
 };
